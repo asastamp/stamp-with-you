f68b914b22b10791fd7ab0d16621016b40813658
5830032121-KN <n_kittiphan@hotmail.com>
2020-05-30 01:06:09 +0700

    add header and sidebar

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
 import { LandingPageRoutingModule } from './landing-page.routing.module';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 @NgModule({
-	declarations: [LandingPageComponent],
+	declarations: [LandingPageComponent, HeaderComponent, SidebarComponent],
 	imports: [CommonModule, LandingPageRoutingModule],
 })
 export class LandingPageModule {}
